197f1e64cff0a0b8513f9a7e7a2feb1b86ed39fd
PIC_Lenovo <picpic2019@gmail.com>
2020-10-30 14:25:37 +0800

    fix job id bug

--- Temp/main.cpp
+++ Temp/main.cpp
@@ -14,10 +14,10 @@ int main() {
 5      JE       03：05     6        
     */
     sim.registerJob(JCB(1, "JA", DateTime(2, 40), 20 * 60));
-    sim.registerJob(JCB(1, "JB", DateTime(2, 50), 30 * 60));
-    sim.registerJob(JCB(1, "JC", DateTime(2, 55), 10 * 60));
-    sim.registerJob(JCB(1, "JD", DateTime(3, 0), 24 * 60));
-    sim.registerJob(JCB(1, "JE", DateTime(3, 5), 6 * 60));
+    sim.registerJob(JCB(2, "JB", DateTime(2, 50), 30 * 60));
+    sim.registerJob(JCB(3, "JC", DateTime(2, 55), 10 * 60));
+    sim.registerJob(JCB(4, "JD", DateTime(3, 0), 24 * 60));
+    sim.registerJob(JCB(5, "JE", DateTime(3, 5), 6 * 60));
     
 
     for (int i = 0; sim.getNow() < DateTime(5, 0); i++) {
